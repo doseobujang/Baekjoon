074d2ac90aecd2dad3ad6588360feb5f932cce53
doseobujang <157891327+doseobujang@users.noreply.github.com>
2025-11-10 22:57:56 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ Python/백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,10 @@
+name = input()
+if name == "NLCS":
+    pname = "North London Collegiate School"
+elif name == "BHA":
+    pname = "Branksome Hall Asia"
+elif name == "KIS":
+    pname = "Korea International School"
+elif name == "SJA":
+    pname = "St. Johnsbury Academy"
+print(pname)
